3e0806b11ea3f727f730e13cef801d35f3b295df
Commander-lol <ljcapitanio@gmail.com>
2015-12-04 03:23:22 +0000

    Removed redundant lib, animated advert

--- routes/advert.js
+++ routes/advert.js
@@ -4,7 +4,7 @@ var express = require('express'),
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.sendFile(path.join(__dirname, "..", "public", "images", "advert.png"));
+    res.sendFile(path.join(__dirname, "..", "public", "images", "advert.gif"));
 });
 
 module.exports = router;
